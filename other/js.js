a65d852fbefb3d7a4a92d1e02b5b01ad0b876310
Aarav Shah <154060537+Ap1311@users.noreply.github.com>
2025-03-09 13:25:58 +0530

    system

---
+++ other/js.js
@@ -0,0 +1,29 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const message = document.getElementById('message');
+    const loginContainer = document.querySelector('.login-container');
+    const loginButton = document.querySelector('button');
+    const body = document.querySelector('body');
+
+    if (username === 'Aarav' && password === 'AP1311') 
+	{
+        message.style.color = 'green';
+        message.textContent = 'Login Successful';
+        loginContainer.classList.remove('error');
+        loginButton.classList.remove('error');
+        body.classList.remove('error');
+		alert("Login Successful");
+		window.location.href="#";
+    } 
+	else 
+	{
+        message.style.color = 'red';
+        message.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/></svg> Login Unsuccessful <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/></svg>';
+        loginContainer.classList.add('error');
+        loginButton.classList.add('error');
+        body.classList.add('error');
+    }
+});
